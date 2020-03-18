929cd3edc65dd5dafe152ddf66b2eac7c0bc1277
Jose <josesz9517@gmail.com>
2020-03-18 17:51:57 -0400

    Readme actualizado y codigito listo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # udemy-linux
+Esto es un proyecto de prueba para Udemy Linux. Esto es para demostrar el uso de git en Ubuntu.
---
+++ codigito.py
@@ -0,0 +1,5 @@
+#Hola bebe esto es una prueba
+
+print ("Hola bebe, como te llamas?")
+name = input ()
+print ("mucho gusto en conocerte " + name)
